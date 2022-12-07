7965f673a83cdb678cf91925ca1c321c5296f38f
LP <lp.passarelli.wilson@gmail.com>
2022-12-07 12:06:55 -0800

    copied in jasmine's code, will go over mine to update it

--- 7-wonders.js
+++ 7-wonders.js
@@ -43,3 +43,44 @@ for (const wonderName of wonderList) {
       console.log(`Error: ${error}`);
     });
 }
+
+//the following is Jasmine's code.
+//Go through this and update your code to be better!
+
+// const axios = require('axios');
+
+// const key = **taking out for jasmine's privacy"
+
+// const API = "https://us1.locationiq.com/v1/search.php"
+
+// const locations = ["Great Wall of China", "Petra", "Colosseum", "Chichen Itza", "Machu Picchu", "Taj Mahal", "Christ the Redeemer"];
+// const locData = {}
+
+// const getInfo = (location) => {
+//     axios.get(API, {params:{key:key, q:location, format:"json"}})
+//         .then((result) => {
+//             const lat = result.data[0].lat;
+//             const lon = result.data[0].lon;
+//             console.log(`${location} lat: ${lat} lon: ${lon}`);
+//         })
+//         .catch((error) => {
+//             console.log(error);
+//         });
+// }
+
+// const getPromise = (time) => {
+//     const timeoutTime = time * 1000;
+//     const myPromise = new Promise((resolve, reject) =>{
+//         setTimeout(() => resolve("It's go time!"), timeoutTime);
+
+//     });
+//     return myPromise;
+// }
+
+// for(let i = 0; i < locations.length; i++) {
+//     const location = locations[i];
+//     const currentPromise = getPromise((i + 1) * 5);
+//     currentPromise.then((value) => {
+//         getInfo(location);
+//     }).catch((error) => {console.log(error)});
+// }
