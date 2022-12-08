5282a91a629eb9dbf84d5c44fc45d294ba314ee2
LP <lp.passarelli.wilson@gmail.com>
2022-12-08 13:46:10 -0800

    typed up Jasmine's solution. it still seems like the first call is working, but the other calls aren't.  not sure why.

--- 7-wonders.js
+++ 7-wonders.js
@@ -2,8 +2,8 @@ const axios = require("axios");
 const dotenv = require("dotenv").config();
 
 const API_KEY = process.env.API_KEY;
-const API = "https://us1.locationiq.com/v1/search";
-const wonderList = [
+const API = "https://us1.locationiq.com/v1/search.php";
+const locations = [
   "Great Wall of China",
   "Petra",
   "Colosseum",
@@ -12,75 +12,62 @@ const wonderList = [
   "Taj Mahal",
   "Christ the Redeemer",
 ];
+const locData = {};
 
-//need to define wonderName.  Will do this in a loop.
-// const params = {
-//   params: {
-//     key: API_KEY,
-//     q: "The Great Wall of China", //wonderName
-//     format: "json",
-//   },
-// };
-
-//the call to the api
-for (const wonderName of wonderList) {
-  console.log(wonderName);
+const getInfo = (location) => {
   axios
-    .get(API, {
-      params: {
-        key: API_KEY,
-        q: wonderName,
-        format: "json",
-      },
-    })
-    .then((response) => {
-      setTimeout(() => {
-        const firstEntry = response.data[0];
-        console.log(wonderName, firstEntry.lat, firstEntry.lon);
-      }, 5000);
+    .get(API, { params: { key: API_KEY, q: location, format: "json" } })
+    .then((result) => {
+      const lat = result.data[0].lat;
+      const lon = result.data[0].lon;
+      console.log(`${location} lat: ${lat} lon: ${lon}`);
+      //add stuff here to add the data to locData?
     })
     .catch((error) => {
-      console.log(`Error: ${error}`);
+      console.log(error);
+    });
+};
+
+const getPromise = (time) => {
+  const timeoutTime = time * 1000;
+  const myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("It's go time!"), timeoutTime);
+  });
+  return myPromise;
+};
+
+for (let i = 0; i < locations.length; i++) {
+  const location = locations[i];
+  const currentPromise = getPromise((i + 1) * 5);
+  currentPromise
+    .then((value) => {
+      getInfo(location);
+    })
+    .catch((error) => {
+      console.log(error);
     });
 }
 
-//the following is Jasmine's code.
-//Go through this and update your code to be better!
-
-// const axios = require('axios');
-
-// const key = **taking out for jasmine's privacy"
-
-// const API = "https://us1.locationiq.com/v1/search.php"
-
-// const locations = ["Great Wall of China", "Petra", "Colosseum", "Chichen Itza", "Machu Picchu", "Taj Mahal", "Christ the Redeemer"];
-// const locData = {}
-
-// const getInfo = (location) => {
-//     axios.get(API, {params:{key:key, q:location, format:"json"}})
-//         .then((result) => {
-//             const lat = result.data[0].lat;
-//             const lon = result.data[0].lon;
-//             console.log(`${location} lat: ${lat} lon: ${lon}`);
-//         })
-//         .catch((error) => {
-//             console.log(error);
-//         });
-// }
-
-// const getPromise = (time) => {
-//     const timeoutTime = time * 1000;
-//     const myPromise = new Promise((resolve, reject) =>{
-//         setTimeout(() => resolve("It's go time!"), timeoutTime);
-
+// LPs' first try (it didn't work.)
+//the call to the api
+// for (const wonderName of locations) {
+//   console.log(wonderName);
+//   axios
+//     .get(API, {
+//       params: {
+//         key: API_KEY,
+//         q: wonderName,
+//         format: "json",
+//       },
+//     })
+//     .then((response) => {
+//       setTimeout(() => {
+//         const firstEntry = response.data[0];
+//         console.log(wonderName, firstEntry.lat, firstEntry.lon);
+//       }, 5000);
+//     })
+//     .catch((error) => {
+//       console.log(`Error: ${error}`);
 //     });
-//     return myPromise;
 // }
-
-// for(let i = 0; i < locations.length; i++) {
-//     const location = locations[i];
-//     const currentPromise = getPromise((i + 1) * 5);
-//     currentPromise.then((value) => {
-//         getInfo(location);
-//     }).catch((error) => {console.log(error)});
-// }
+//let's try with helper functions.
