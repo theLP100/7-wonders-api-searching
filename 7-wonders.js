dc25fe783fedd37aefbc341d85cd87f7479c38e6
LP <lp.passarelli.wilson@gmail.com>
2022-12-13 09:16:47 -0800

    fixed the bug by changing https to http.  jasmine's code and my code are both working yay.

--- 7-wonders.js
+++ 7-wonders.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const dotenv = require("dotenv").config();
 
 const API_KEY = process.env.API_KEY;
-const API = "https://us1.locationiq.com/v1/search.php";
+const API = "http://us1.locationiq.com/v1/search.php";
 const locations = [
   "Great Wall of China",
   "Petra",
@@ -14,60 +14,66 @@ const locations = [
 ];
 const locData = {};
 
-const getInfo = (location) => {
-  axios
-    .get(API, { params: { key: API_KEY, q: location, format: "json" } })
-    .then((result) => {
-      const lat = result.data[0].lat;
-      const lon = result.data[0].lon;
-      console.log(`${location} lat: ${lat} lon: ${lon}`);
-      //add stuff here to add the data to locData?
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-};
+//both of these work!!!!
 
-const getPromise = (time) => {
-  const timeoutTime = time * 1000;
-  const myPromise = new Promise((resolve, reject) => {
-    setTimeout(() => resolve("It's go time!"), timeoutTime);
-  });
-  return myPromise;
-};
+//jasmine's code:
 
-for (let i = 0; i < locations.length; i++) {
-  const location = locations[i];
-  const currentPromise = getPromise((i + 1) * 5);
-  currentPromise
-    .then((value) => {
-      getInfo(location);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-}
-
-// LPs' first try (it didn't work.)
-//the call to the api
-// for (const wonderName of locations) {
-//   console.log(wonderName);
+// const getInfo = (location) => {
 //   axios
-//     .get(API, {
-//       params: {
-//         key: API_KEY,
-//         q: wonderName,
-//         format: "json",
-//       },
-//     })
-//     .then((response) => {
-//       setTimeout(() => {
-//         const firstEntry = response.data[0];
-//         console.log(wonderName, firstEntry.lat, firstEntry.lon);
-//       }, 5000);
+//     .get(API, { params: { key: API_KEY, q: location, format: "json" } })
+//     .then((result) => {
+//       const lat = result.data[0].lat;
+//       const lon = result.data[0].lon;
+//       console.log(`${location} lat: ${lat} lon: ${lon}`);
+//       //add stuff here to add the data to locData?
 //     })
 //     .catch((error) => {
-//       console.log(`Error: ${error}`);
+//       console.log(
+//         `there has been an error in getInfo for ${location}. Cause: ${error}`
+//       );
+//     });
+// };
+
+// const getPromise = (time) => {
+//   const timeoutTime = time * 1000;
+//   const myPromise = new Promise((resolve, reject) => {
+//     setTimeout(() => resolve("It's go time!"), timeoutTime);
+//   });
+//   return myPromise;
+// };
+
+// for (let i = 0; i < locations.length; i++) {
+//   const location = locations[i];
+//   const currentPromise = getPromise((i + 1) * 3);
+//   currentPromise
+//     .then((value) => {
+//       getInfo(location);
+//     })
+//     .catch((error) => {
+//       console.log(`there has been an error in the for loop: ${error}`);
 //     });
 // }
+
+// LP's code
+//the call to the api
+for (const wonderName of locations) {
+  console.log(wonderName);
+  axios
+    .get(API, {
+      params: {
+        key: API_KEY,
+        q: wonderName,
+        format: "json",
+      },
+    })
+    .then((response) => {
+      setTimeout(() => {
+        const firstEntry = response.data[0];
+        console.log(wonderName, firstEntry.lat, firstEntry.lon);
+      }, 5000);
+    })
+    .catch((error) => {
+      console.log(`Error: ${error}`);
+    });
+}
 //let's try with helper functions.
